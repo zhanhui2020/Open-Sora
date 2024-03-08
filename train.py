9b312a53ae7c634c5cd941e9a33bcef41638fdce
zhanhui2020 <809755893@qq.com>
2024-03-08 16:28:20 +0800

    start to read the code

--- train.py
+++ train.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
+# note: peter wu by 2024-03-08
+
 """
 A minimal training script for DiT using PyTorch DDP.
 """
